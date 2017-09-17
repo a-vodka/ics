d070428e2f79d7f239099eafb840e5b086f5c52a
oleksii <a_vodka@mail.ru>
2017-09-17 23:32:51 +0300

    lection3 and lab4

---
+++ lab4/ExcelAutomation/ExcelAutomation/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelAutomation
+{
+    using Excel = Microsoft.Office.Interop.Excel;
+    using Word = Microsoft.Office.Interop.Word;
+
+    public partial class Form1 : Form
+    {
+        private Excel.Application excelApp;
+        private Word.Application wordApp;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            excelApp = new Excel.Application();
+            wordApp = new Word.Application();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            excelApp.Quit();
+            wordApp.Quit();
+        }
+
+        private void ExcelButton_Click(object sender, EventArgs e)
+        {
+            excelApp.Visible = true;
+            Excel.Workbook wb = excelApp.Workbooks.Add();
+            Excel.Worksheet sheet = wb.ActiveSheet;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    sheet.Cells[i, j].Value = i * j;
+                    if (i == j)
+                        sheet.Cells[i, j].Font.Bold = true;
+                }
+            }
+
+            sheet.Range["A1:J1"].Font.Bold = true;
+            sheet.Range["A1:A10"].Font.Bold = true;
+            sheet.Range["A1:J1"].Interior.Color = Color.LightGray;
+            sheet.Range["A1:A10"].Interior.Color = Color.LightGray;
+
+            float left = (float)sheet.Range["A15"].Left;
+            float top = (float)sheet.Range["A15"].Top;
+
+            sheet.Shapes.AddPicture(Application.StartupPath + @"/../../logo.png", Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoCTrue, left, top, 100, 100);
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wordApp.Visible = true;
+            Word.Document wd = wordApp.Documents.Add();
+            wd.Range().Text = "Hello word!";
+            wd.Range().Font.Size = 64;
+        }
+    }
+}
+
+
---
+++ lab4/Excel_graph/Excel_graph/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Excel_graph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+Excel.Application excelApp = new Excel.Application();
+excelApp.Visible = true;
+            
+Excel.Workbook wb = excelApp.Workbooks.Add();
+Excel.Worksheet sheet = wb.ActiveSheet;
+
+//add data
+sheet.Cells[1, 1] = "";
+sheet.Cells[1, 2] = "Student1";
+sheet.Cells[1, 3] = "Student2";
+sheet.Cells[1, 4] = "Student3";
+sheet.Cells[2, 1] = "Term1";
+sheet.Cells[2, 2] = "80";
+sheet.Cells[2, 3] = "65";
+sheet.Cells[2, 4] = "45";
+sheet.Cells[3, 1] = "Term2";
+sheet.Cells[3, 2] = "78";
+sheet.Cells[3, 3] = "72";
+sheet.Cells[3, 4] = "60";
+sheet.Cells[4, 1] = "Term3";
+sheet.Cells[4, 2] = "82";
+sheet.Cells[4, 3] = "80";
+sheet.Cells[4, 4] = "65";
+sheet.Cells[5, 1] = "Term4";
+sheet.Cells[5, 2] = "75";
+sheet.Cells[5, 3] = "82";
+sheet.Cells[5, 4] = "68";
+
+Excel.Range chartRange = sheet.get_Range("A1", "d5");
+Excel.ChartObjects xlCharts = (Excel.ChartObjects)sheet.ChartObjects();
+Excel.ChartObject myChart = (Excel.ChartObject)xlCharts.Add(10, 80, 300, 250);
+Excel.Chart chartPage = myChart.Chart;
+chartPage.ChartWizard(chartRange, Excel.XlChartType.xlColumnClustered, Title:"Diagram title");
+
+chartPage.Export(Application.StartupPath + @"./excel_chart_export.png", "png");
+        }
+    }
+}
